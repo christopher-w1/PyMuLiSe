89bb0e45d23375b8c634b4d14eca52e2ad0ecb1b
Christopher Widling <christopher.widling@gmail.com>
2025-05-21 21:18:15 +0200

    test for song model

---
+++ src/test_song.py
@@ -0,0 +1,19 @@
+from modules.model_song import Song
+from modules.utils import find_song_paths
+music_dir = "./music"
+
+
+
+
+# Find all song paths in the music directory recursively
+song_paths = find_song_paths(music_dir)
+print(f"Found {len(song_paths)} songs in {music_dir}")
+
+song_objects = []
+for song_path in song_paths:
+    print(f"Found song: {song_path}")
+    song = Song(song_path)
+    song.pretty_print()
+    song_objects.append(song)
+
+            
